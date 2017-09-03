a27b3c98bc12d43dd469b2acc3e2255263e0473b
Philip Meier <philip.meier06@googlemail.com>
2017-09-03 11:57:55 +0100

    Add click handlers to board buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,17 +11,6 @@ function setupModeSelectionPage() {
   button.addEventListener('click', startGame);
 }
 
-function startGame() {
-  addBoard({dimension: 3, marks: ['', '', '', '', '', '', '', '', '']});
-  window.location.hash = '#game';
-}
-
-function addBoard(board) {
-  var boardElement = document.querySelector('div.board');
-  var boardHTML = generateBoardHTML(board);
-  boardElement.innerHTML = boardHTML;
-}
-
 function renderCurrentLocation() {
   render(decodeURI(window.location.hash));
 }
@@ -82,19 +71,41 @@ function removeClass(element, className) {
   }
 }
 
+function startGame() {
+  addBoard({dimension: 3, marks: ['', '', '', '', '', '', '', '', '']});
+  window.location.hash = '#game';
+}
+
+function addBoard(board) {
+  var boardElement = document.querySelector('div.board');
+  var boardHTML = generateBoardHTML(board);
+  boardElement.innerHTML = boardHTML;
+  var buttons = boardElement.querySelectorAll('button.btn-cell');
+  for (var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', bindClick(buttons[i]));
+  }
+}
+
+function bindClick(button)
+{
+  return function() {
+    button.innerHTML = button.value;
+  }
+}
+
+
 function generateBoardHTML(board) {
-  var out = '<form>';
+  var out = '';
   for (row = 0; row < board.dimension; row++) {
     out = out + '<div class="row">';
     for (col = 0; col < board.dimension; col++) {
       index = row*board.dimension + col;
       mark = board.marks[index]
       space = index + 1
-      out = out + ' <button class="btn cell btn-cell" type="submit" name="move" value="' +
+      out = out + ' <button class="btn cell btn-cell" name="move" value="' +
                   space + '">' + mark + '</button>';
     }
     out = out + '</div>';
   }
-  out + '</form>';
   return out; 
 }

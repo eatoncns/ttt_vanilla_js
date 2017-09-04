36a5cb85090de8208c51c003f872fa93704f4428
Philip Meier <philip.meier06@googlemail.com>
2017-09-04 21:47:03 +0100

    Extract GameOptions into separate file

--- assets/js/script.js
+++ assets/js/script.js
@@ -122,45 +122,8 @@ var Game = (function() {
   return me;
 }());
 
-var ModeSelection = (function() {
-  var settings = {
-    page: document.querySelector('div.mode-selection'),
-    gameOptions: document.querySelector('form.game-options'),
-    button: document.querySelector('input.new-game')
-  };
-  
-  var me = {};
 
-  function setupBindings() {
-    settings.button.addEventListener('click', startGame);
-  }
-
-  function startGame() {
-    var mode = settings.gameOptions.elements['mode'].value;
-    var board_dimension = settings.gameOptions.elements['board_dimension'].value;
-    postAjax('http://localhost:4567/api/new-game', { board_dimension: board_dimension, mode: mode}, function(data) {
-      var board = JSON.parse(data);
-      Game.init(board);
-      window.location.hash = '#game';
-    });
-  }
-
-  me.init = function() {
-    setupBindings();
-  }
-
-  me.setVisible = function() {
-    removeClass(settings.page, 'invisible');
-  }
-
-  me.setInvisible = function() {
-    addClass(settings.page, 'invisible');
-  }
-
-  return me;
-}());
-
-ModeSelection.init();
+GameOptions.init();
 renderCurrentLocation();
 
 
@@ -174,7 +137,7 @@ function render(url) {
 }
 
 function setPagesInvisible() {
-  ModeSelection.setInvisible();
+  GameOptions.setInvisible();
   Game.setInvisible();
 }
 
@@ -182,7 +145,7 @@ function displayPage(url) {
   var pageUrl = url.split('/')[0];
   var map = {
     '': function() {
-      ModeSelection.setVisible();
+      GameOptions.setVisible();
     },
     '#game' : function() {
       Game.setVisible();

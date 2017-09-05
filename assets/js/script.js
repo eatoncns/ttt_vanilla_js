5ab1f82cd1ce5dbbd82e80a5baf56d641647ae83
Philip Meier <philip.meier06@googlemail.com>
2017-09-05 10:34:46 +0100

    Remove visibility stuff from script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,44 +34,6 @@ function displayPage(url) {
   }
 }
 
-var Visibility = (function() {
-  var me = {};
-
-  function hasClass(element, className) {
-    element.classList ? element.classList.contains(className) 
-      : new RegExp('\\b' + className + '\\b').test(element.className);
-  }
-
-  function addClass(element, className) {
-    if (element.classList) {
-      element.classList.add(className);
-    }
-    else if (!hasClass(element, className)) {
-      element.className += ' ' + className;
-    }
-  }
-
-  function removeClass(element, className) {
-    if (element.classList) {
-      element.classList.remove(className);
-    }
-    else {
-      classNameRegExp = new RegExp('\\b' + className + '\\b', 'g');
-      element.className = element.className.replace(classNameRegExp, '');
-    }
-  }
-
-  me.setVisible = function(element) {
-    removeClass(element, 'invisible');
-  }
-
-  me.setInvisible = function(element) {
-    addClass(element, 'invisible');
-  }
-
-  return me;
-}());
-
 function postAjax(url, data, success) {
   var params = Object.keys(data).map(
     function(k){ return encodeURIComponent(k) + '=' + encodeURIComponent(data[k])  }

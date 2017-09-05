f34c5ae43021102d73e0d58f5de057a2b78d0b4a
Philip Meier <philip.meier06@googlemail.com>
2017-09-05 10:37:00 +0100

    Move ajax function into separate file

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,20 +33,3 @@ function displayPage(url) {
     map[pageUrl]();
   }
 }
-
-function postAjax(url, data, success) {
-  var params = Object.keys(data).map(
-    function(k){ return encodeURIComponent(k) + '=' + encodeURIComponent(data[k])  }
-  ).join('&');
-
-  var xhr = new XMLHttpRequest();
-  xhr.open('POST', url);
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState>3 && xhr.status==200) { success(xhr.responseText);  }
-
-  };
-  xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-  xhr.send(params);
-  return xhr;
-}
